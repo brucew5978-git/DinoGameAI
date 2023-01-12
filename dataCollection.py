f43ed445a30202fa2fb2f048c7e9fda7eb721c6f
brucew5978-git <83440706+brucew5978-git@users.noreply.github.com>
2023-01-11 22:42:51 -0500

    Add files via upload
    2023-1-11 Simplified

--- dataCollection.py
+++ dataCollection.py
@@ -1,50 +1,11 @@
 import matplotlib.pyplot as plt
 import pyautogui as pg
 import time
-import keyboard
+from pynput import keyboard
 
 class CollectorError(Exception):
-    pass    
+    pass
 
-class CircularQueue:
-
-    def __init__(self, length):
-        self.length = length
-        self.items = [None] * n
-        self.size = 0
-        self.start = 0
-
-    def enqueue(self, item):
-        if self.isFull():
-            return False
-        
-        self.items[self.getEnd()] = item
-        self.size+=1
-
-    def dequeue(self):
-        if self.isEmpty():
-            return False
-
-        dequeueValue = self.items[self.start]
-        self.items[self.start]=None
-        self.start=(self.start+1) % self.length
-        self.size-=1
-        return dequeueValue
-
-    def isFull(self):
-        return self.size==self.length
-
-    def isEmpty(self):
-        return self.size==0
-
-    def getEnd(self):
-        return (self.start+self.size)%self.length
-
-    def clear(self):
-        self.items=[None]*self.length
-        self.size=0
-        self.start=0
-    
 
 def findScreen():
     print("In Find Screen")
@@ -68,75 +29,42 @@ def findScreen():
 
     raise CollectorError('Screen not found...')
 
-class KeyboardDetector:
+def frequencyPhotoTaker():
+    return 0
 
-    def getState(self):
-        
-        state=''
-        if keyboard.is_pressed('up'):
-            state+='u'
+def on_press(key):
+    try:
 
-        if keyboard.is_pressed('down'):
-            state+='d'
-
-        return state
-
-class DataCollector:
-
-    def __init__(self, lastImageID=0, detector=KeyboardDetector()):
-        #lastImageID == last image saved to a file
-
-        print('[dataCollector.INIT]')
-        time.sleep(5)
-
-        dimensions=findScreen()
-        self.detector = detector
-        self.ID=lastImageID
-
-        self.dimensions = (dimensions['screenLeft'], dimensions['screenTop'], dimensions['screenRight'], dimensions['screenBottom'])
-        print(self.dimensions)
-    
-    def screenshot(self):
-        img = pg.screenshot()
-        img = img.crop(self.dimensions)
-        img=img.resize((238,412))
-        return img
-
-    def getGameState(self):
-        return self.detector.getState()
+        if(key.char == "w"):
+            print("Jump detected")
 
 
-class PDataCollector(DataCollector):
 
-    def __init__(self, hotkey='w', lastImageID=0, detector=KeyboardDetector()):
-        super(PDataCollector, self).__init__(lastImageID, detector)
-        self.isPressed = False
-        self.hotkey = hotkey
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
 
-    def run(self, root='data'):
-        if keyboard.is_pressed(self.hotkey) and not self.isPressed:
-            self.isPressed = True
-            img = self.screenshot()
-            imageID='{:06d}'.format(self.ID)
-            imageName = f'{root}/images/{imageID}.jpg'
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
 
-            print(imageName)
-            img.save(imageName)
-            self.ID+=1
-        
-        elif not keyboard.is_pressed(self.hotkey):
-            self.isPressed = False
 
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+listener.start()
 
 #dataCollector = PDataCollector(lastImageID=0, hotkey='w')
 
-keyboard.add_hotkey('a', lambda: keyboard.write('Geek'))
-keyboard.add_hotkey('ctrl + shift + a', print, args =('you entered', 'hotkey'))
-  
-keyboard.wait('esc')
-
+'''
 while True:
-    '''if keyboard.is_pressed('0x31'):
+   if keyboard.is_pressed('0x31'):
         print('ending...')
         break
     '''
@@ -144,9 +72,7 @@ while True:
     #dataCollector.run()
 
 #Find screen
+#Press key detection function and takes a photo
+#Periodic photo taker
 
-#Press key detection function
-#File write function for storing screenshots
-#Circular queue for storing imgs
-#Use circular queue to backtrack where model should jump
-
+#File write function for going through data and creating csv for sorting
